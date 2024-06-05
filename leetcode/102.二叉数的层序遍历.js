75ed9e77cad11ae96a50bbaac28b0e79f3eec714
wuduo0701 <linjiahao7@jd.com>
2024-06-05 14:18:09 +0800

    feat: leetcode215.二叉树的层序遍历

---
+++ leetcode/102.二叉数的层序遍历.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=javascript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  // 递归-方法
+  // if (!root) return []
+  // const result = []
+
+  // function order(node, level = 0) {
+  //   if (!node) return
+
+  //   if (!result[level]) result[level] = []
+  //   result[level].push(node.val)
+  //   order(node.left, level + 1)
+  //   order(node.right, level + 1)
+  // }
+  // order(root, 0)
+  // return result
+
+  // 迭代的方法
+  if (!root) return []
+  const result = []
+  const queue = [root]
+  while (queue.length) {
+    const queueLen = queue.length
+    const nodeLevel = []
+
+    for (let i = 0; i < queueLen; i++) {
+      let node = queue.shift()
+      nodeLevel.push(node.val)
+
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+    }
+    result.push(nodeLevel)
+  }
+  return result
+}
+// @lc code=end
--- leetcode/BinaryTree/index.js
+++ leetcode/BinaryTree/index.js
@@ -37,7 +37,7 @@ class BinaryTree {
     }
   }
 
-  // 中序遍历
+  // 中序遍历 (值 -> 左子数 -> 右子数)
   inorderTraversal(node, result = []) {
     if (node !== null) {
       this.inorderTraversal(node.left, result)
@@ -47,7 +47,7 @@ class BinaryTree {
     return result
   }
 
-  // 前序遍历
+  // 前序遍历 (左子数  -> 值 -> 右子数)
   preorderTraversal(node, result = []) {
     if (node !== null) {
       result.push(node.value)
@@ -57,12 +57,44 @@ class BinaryTree {
     return result
   }
 
-  // 后序遍历
+  // 后序遍历 (左子数 -> 右子数 -> 值)
   postorderTraversal(node, result = []) {
     if (node !== null) {
       this.postorderTraversal(node.left, result)
       this.postorderTraversal(node.right, result)
       result.push(node.value)
+    }
+    return result
+  }
+
+  // 层序遍历 (一层层遍历) - 递归
+  // 广度优先遍历
+  levelOrderTraversal(node, result = [], level = 0) {
+    if (node !== null) {
+      result[level] = result[level] || []
+      result[level].push(node.value)
+
+      this.levelOrderTraversal(node.left, result, level + 1)
+      this.levelOrderTraversal(node.right, result, level + 1)
+    }
+    return result
+  }
+  // 层序遍历 - 迭代
+  levelOrderTraversal2(root, result = []) {
+    if (root !== null) {
+      const queue = [root]
+      while (queue.length) {
+        const queueLen = queue.length
+        const levelNode = [] // 这个层级存的数
+        for (let i = 0; i < queueLen; i++) {
+          const node = queue.shift()
+          levelNode.push(node.value)
+
+          if (node.left) queue.push(node.left) // 左子数
+          if (node.right) queue.push(node.right) // 右子数
+        }
+        result.push(levelNode)
+      }
     }
     return result
   }
@@ -84,5 +116,7 @@ tree.insert(13)
 
 console.log(tree)
 console.log('Inorder Traversal:', tree.inorderTraversal(tree.root))
-console.log('Inorder Traversal:', tree.preorderTraversal(tree.root))
-console.log('Inorder Traversal:', tree.postorderTraversal(tree.root))
+console.log('preorder Traversal:', tree.preorderTraversal(tree.root))
+console.log('postorder Traversal:', tree.postorderTraversal(tree.root))
+console.log('levelOrder Traversal:', tree.levelOrderTraversal(tree.root))
+console.log('levelOrder Traversal:', tree.levelOrderTraversal2(tree.root))
