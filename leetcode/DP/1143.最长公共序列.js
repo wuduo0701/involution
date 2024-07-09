b1087ac046c783e542e49beccadc65476bc10ca7
Wu Duo <1970186643@qq.com>
2024-07-09 20:39:01 +0800

    feat: leetcode 1143

--- leetcode/DP/1143.最长公共序列.js
+++ leetcode/DP/1143.最长公共序列.js
@@ -29,5 +29,26 @@
  * @param {string} text2
  * @return {number}
  */
-var longestCommonSubsequence = function (text1, text2) {}
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length
+  const n = text2.length
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0))
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[m][n]
+}
 // @lc code=end
+
+// NOTE: 笔记
+// 1、创建二维数组
+// 方法一（from + fill）：Array.from(new Array(10), () => new Array.fill(0))
+// 方法二（fill + map）：new Array(10).fill(0).map(() => new Array(10).fill(0))
+
+// 方法三（不能用）：new Array(10).fill(new Array(10).fill(0)) // FIXME:这个会导致二维数组里是引用类型，改一处会全改
