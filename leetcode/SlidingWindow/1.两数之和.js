c1a0f14f6b91644632b48825cc8be0426f273d93
Wu Duo <1970186643@qq.com>
2024-08-04 23:53:28 +0800

    polish: 优化代码

--- leetcode/SlidingWindow/1.两数之和.js
+++ leetcode/SlidingWindow/1.两数之和.js
@@ -28,9 +28,12 @@
  */
 var twoSum = function (nums, target) {
   if (nums.length <= 1) return []
-  let map = {}
-
+  let map = {} // 记住需要的num及其此时数的下标。
+  // 举例：如所需 target = m + n
+  // 1. 则 needNum = target - m， value 为 m 的下标
+  // 2. 则只需要判断map[needNum]是否为空即可，因为这个找的就是 n。如之前map已经存过这个数，则代表之前遇到另一半的数了，返回[map[needNum], i]即可
   for (let i = 0; i < nums.length; i++) {
+    // 找出需要的数 needNum
     let needNum = target - nums[i]
     if (map[needNum] === undefined) {
       map[nums[i]] = i
