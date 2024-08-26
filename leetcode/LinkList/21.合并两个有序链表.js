33f761feee5faa4ee70ec95b7391509bd568f440
Wu Duo <1970186643@qq.com>
2024-08-26 10:57:09 +0800

    feat: [21] 合并两个有序链表

---
+++ leetcode/LinkList/21.合并两个有序链表.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ *
+ * https://leetcode.cn/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (66.53%)
+ * Likes:    3512
+ * Dislikes: 0
+ * Total Accepted:    1.7M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ *
+ *
+ * 示例 3：
+ *
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+  const dummy = new ListNode() // 虚拟头节点
+  let cur = dummy // 新节点
+  // 迭代
+  // 1. 比较两个链表的节点值，将较小的节点添加到 dummy 的末尾
+  // 2. 直到其中一个链表为空
+  // 3. 将另一个链表剩余部分添加到 dummy 的末尾。
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      cur.next = list1
+      list1 = list1.next
+    } else {
+      cur.next = list2
+      list2 = list2.next
+    }
+    cur = cur.next
+  }
+  cur.next = list1 || list2
+  return dummy.next
+}
+// @lc code=end
